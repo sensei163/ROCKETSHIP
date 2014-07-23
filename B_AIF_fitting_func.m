5788b11b21fbf2f65065cb750a78c86c7f749f69
Thomas Ng <thomasn@caltech.edu>
2014-07-22 19:17:34 -0700

    20140722 Run B, added manual time vector capability

--- B_AIF_fitting_func.m
+++ B_AIF_fitting_func.m
@@ -1,4 +1,4 @@
-function results = B_AIF_fitting_func(results_a_path,start_time,end_time,start_injection,end_injection,fit_aif,import_aif_path,time_resolution)
+function results = B_AIF_fitting_func(results_a_path,start_time,end_time,start_injection,end_injection,fit_aif,import_aif_path,time_resolution, timevectpath)
 
 % B_AIF_fitting_func - This file is used to apply a model fitting to the
 % arterial input function. AIF is fit to a bi-exponential model with a
@@ -15,13 +15,15 @@ function results = B_AIF_fitting_func(results_a_path,start_time,end_time,start_i
 %  fit_aif            - fit AIF to model (bool, 0 for reference region)
 %  import_aif_path    - import AIF from .mat file (string)
 %  time_resolution    - time resolution of dynamic scan (in minutes)
+%  timevectpath       - import manual time vector from .mat file (string)
 %
 %
 % The script loads the data arrays generated from A_make_R1maps_func().
 % Then one can
 %
 % a) fit to a model.
-% b) limit the time interval from which further data fitting is performed.
+% b) limit the time interval from which further data fitting is performed,
+% as well as define a unique time vector.
 % c) There is an option import a population or averaged AIF from multiple
 % datasets should you desire. This may be useful if you want to get better
 % SNR or comparing multiday studies.
@@ -39,7 +41,7 @@ function results = B_AIF_fitting_func(results_a_path,start_time,end_time,start_i
 %
 % Thomas Ng
 % Caltech
-% December 2011
+% December 2011, Jun 2014
 % Updated Dec 2013 Sam Barnes
 
 %% Toggle options
@@ -93,8 +95,15 @@ disp('User selected fit AIF to model: ');
 disp(fit_aif);
 disp('User selected import AIF path: ');
 fprintf('%s\n\n',import_aif_path);
-disp('User selected time resolution (sec)');
-disp(time_resolution*60);
+
+if ~isempty(timevectpath)
+    disp('User selected import time vector path: ');
+    fprintf('%s\n\n',timevectpath);
+else
+    disp('User selected time resolution (sec)');
+    disp(time_resolution*60);
+end
+
 if quant
     disp('User selected quantification: ie. use T1 maps');
 else
@@ -110,8 +119,45 @@ tic
 %% 2. time management (Change the starter, timeend if needed)
 %% b) Time management
 
-%Generate time curve
-timer   = 0:time_resolution:time_resolution*(size(Cp,1)-1);
+if ~isempty(timevectpath)
+    external = load(timevectpath);
+    if isfield(external,'timer')
+        timer = external.timer;
+        
+        if length(timer) ~= prod(size(timer))
+            disp('Time vector not a vector');
+            return
+        end
+    else
+        disp('No time vector found in selected file')
+        return
+    end
+    
+    % Check that time vector is same size as data. If shorter, extrapolate
+    % using the last time interval as guide.
+
+    if length(timer) > size(Cp,1)
+        timer = timer(1:size(Cp,1));
+    elseif length(timer) < size(Cp,1)
+        lasttimeres = timer(end)-timer(end-1);
+        
+        while length(timer < size(Cp,1))
+            timer(end+1) = timer(end)+lasttimeres;
+        end
+    else
+        disp('Imported time vector same size as data');
+    end
+    
+    if max(timer) > 100
+        % Most likely in seconds, convert to minutes
+        disp('Imported time vector appears to be in seconds, converting to minutes');
+        timer = timer./60;
+    end
+else   
+    %Generate time curve
+    timer   = 0:time_resolution:time_resolution*(size(Cp,1)-1);
+
+end
 
 %Restrict to specified times
 if(start_time)
@@ -192,24 +238,14 @@ else
     external = load(import_aif_path);
     if isfield(external,'Cp_use')
         Cp_use = external.Cp_use;
-        Stlv_use = external.Stlv_use;       % FIXME You can't do this, need to check if exists
+        Stlv_use = external.Stlv_use;
     elseif isfield(external,'Bdata')
         if isfield(external.Bdata,'Cp_use')
             Cp_use = external.Bdata.Cp_use;
-        elseif isfield(external.Bdata,'xdata')
-            Cp_use = external.Bdata.xdata{1}.Cp;
-        else
-            disp('No Cp curve found in selected file')
-            return
-        end
-        
-        if isfield(external.Bdata,'Stlv_use')
             Stlv_use = external.Bdata.Stlv_use;
         elseif isfield(external.Bdata,'xdata')
+            Cp_use = external.Bdata.xdata{1}.Cp;
             Stlv_use = external.Bdata.xdata{1}.Stlv;
-        else
-            disp('No STLV curve found in selected file')
-            return
         end
     elseif isfield(external,'xdata')
         Cp_use = external.xdata{1}.Cp;
@@ -218,28 +254,6 @@ else
         disp('No Cp curve found in selected file')
         return
     end
-    
-    % Line up imported AIF with current data
-    if isfield(external,'Bdata')
-        imported_injection_time = external.Bdata.start_injection/external.Bdata.time_resolution;
-        current_injection_time = start_injection/time_resolution;
-        
-        shift_by = round(current_injection_time - imported_injection_time);
-        
-        if shift_by>0
-            shifted_cp = [ones(1,shift_by)*Cp_use(1) Cp_use(1:end-shift_by)];
-            Cp_use = shifted_cp;
-        elseif shift_by<0
-            shift_by = abs(shift_by);
-            shifted_cp = [Cp_use(1+shift_by:end) ones(1,shift_by)*Cp_use(end)];
-            Cp_use = shifted_cp;
-        end
-        
-        fprintf('AIF Shifted by %d images\n',shift_by);
-    else
-        warning('cannot line up imported AIF with current data')
-    end
-    
     % Check length, if different try applying time constraints
     if numel(timer)<numel(Cp_use)
         Cp_use = Cp_use(start_time:end_time);
