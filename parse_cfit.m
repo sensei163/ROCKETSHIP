38e015cf3bc7fcab4b29a1742e93f899d03c9f80
Thomas Ng <thomasn@caltech.edu>
2014-12-17 05:27:43 -0800

    20141217: Fixed parse_cfit.m to ensure appropriate handling of cfit object Updated default dce_preferences.txt file to expand limit bounds for paper.

--- parse_cfit.m
+++ parse_cfit.m
@@ -1,10 +1,11 @@
-function [fitting_results, residuals] = parse_cfit(cfit_fit, cfit_gof, cfit_output, cur_dce_model, numparam, numtimepoints, quant)
+function [fitting_results, residuals] = parse_cfit(cfit_fit, cfit_gof, cfit_output, cur_dce_model, numparam, quant)
 
 numobjects = numel(cfit_fit);
+numresiduals = cfit_output{1}.numobs;
 
 %Preallocate for speed
 fitting_results = zeros([numobjects, numparam], 'double');
-residuals       = zeros([numobjects, numtimepoints], 'double');
+residuals       = zeros([numobjects, numresiduals], 'double');
 
 parfor i = 1:numobjects
     [fitting_results(i,:), residuals(i,:)] = parse_cfit_helper(cfit_fit{i}, cfit_gof{i}, cfit_output{i}, cur_dce_model, quant, residuals(i,:));
